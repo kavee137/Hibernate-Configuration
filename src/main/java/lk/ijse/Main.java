f581b574bfea1e224a8e8cca5e9b56e9a57b70c0
kavee137 <kaveesharukshan137@gmail.com>
2024-08-22 21:58:37 +0530

    delete done

--- src/main/java/lk/ijse/Main.java
+++ src/main/java/lk/ijse/Main.java
@@ -9,18 +9,20 @@ import org.hibernate.Transaction;
 public class Main {
     public static void main(String[] args) {
         Session session = FactoryConfiguration.getInstance().getSession();
-
-        FullName fullName = new FullName("Kaveesha", "Rukshan");
-
+        FullName name = new FullName("Kaveesha","Rukshan");
         Student student = new Student();
-        student.setId(1);
-        student.setName(fullName);
-        student.setAddress("Agalawatta");
-        //open transaction
+        student.setId(3);
+        student.setName(name);
+        student.setAddress("Galle");
+
         Transaction transaction = session.beginTransaction();
 
+//        save
         session.save(student);
-        //delete, get another table
+//        delete
+//        session.delete(student);
+        Student student1 = session.get(Student.class,1);
+        System.out.println(student1.toString());
         transaction.commit();
         session.close();
     }
